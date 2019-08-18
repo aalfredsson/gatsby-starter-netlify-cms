a4ea0dac77b51cacc6bbd6997d0abb6a1e7ecb32
Axel Alfredsson <aalfredsson94@gmail.com>
2019-08-18 12:03:25 +0200

    Add new list page directive. Will support pages where a sidenavigation is present and you can add new pages with this navigation as a part of it and your own data on the page with the navigation linking to it.

---
+++ src/templates/list-page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+const ListsPage = ( {data} ) => {
+    const { markdownRemark: post } = data;
+
+    return(
+      <div className="section">
+        <h2 className="title is-size-3 has-text-weight-bold is-bold-light">{post.frontmatter.title}</h2>
+        <p>{post.frontmatter.description}</p>
+      </div>
+    );
+};
+
+export default ListsPage;
+
+export const ListsPageQuery = graphql`
+query ListsPage($id: String!) {
+  markdownRemark(id: {eq: $id}) {
+    frontmatter {
+      title
+      templateKey
+      description
+    }
+  }
+}
+`;
